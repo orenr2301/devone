e821fd9af2163eb8150f102c1de0675830131f82
orenr2301 <orenrahav13@yahoo.com>
2023-06-12 01:16:26 +0300

    comment v1

--- node-app/app.js
+++ node-app/app.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
       const html = `<h1>Quantity of Apples: ${applesQty}</h1>`;
       res.send(html);
 
-      // Close the MongoDB connection
+      // Close the MongoDB connection gp
       client.close();
     });
   });
