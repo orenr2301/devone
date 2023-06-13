f5ccda47a3f6e4b505ef284c24195bee29d09e2b
orenr2301 <orenrahav13@yahoo.com>
2023-06-13 05:45:38 +0300

    sss

--- ecr/backend.tf
+++ ecr/backend.tf
@@ -19,8 +19,8 @@ terraform {
 
 
 provider "aws" {
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
+    /* access_key = var.aws_access_key
+    secret_key = var.aws_secret_key */
     region = "eu-central-1"
     profile = "default"
 }
